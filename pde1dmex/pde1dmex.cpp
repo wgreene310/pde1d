49971dc5660a9be2fbf45092b74a994668d589df
wgreene <w.h.greene@gmail.com>
2020-01-27 13:42:06 -0500

    This appears to make cout work with mex.

--- pde1dmex/pde1dmex.cpp
+++ pde1dmex/pde1dmex.cpp
@@ -35,6 +35,24 @@ using std::endl;
 typedef double Float;
 
 namespace {
+
+  class mystream : public std::streambuf
+  {
+  protected:
+    virtual std::streamsize xsputn(const char* s, std::streamsize n) { mexPrintf("%.*s", n, s); return n; }
+    virtual int overflow(int c = EOF) { if (c != EOF) { mexPrintf("%.1s", &c); } return 1; }
+  };
+  class scoped_redirect_cout
+  {
+  public:
+    scoped_redirect_cout() { old_buf = std::cout.rdbuf(); std::cout.rdbuf(&mout); }
+    ~scoped_redirect_cout() { std::cout.rdbuf(old_buf); }
+  private:
+    mystream mout;
+    std::streambuf* old_buf;
+  };
+  scoped_redirect_cout mycout_redirect;
+
   template<class T>
   mxArray *toMxArray(const T &a)
   {
